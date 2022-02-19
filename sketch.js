2df0dc460382b18cb7d40f25546d29a8ec95188d
shrey8888 <100024327+shrey8888@users.noreply.github.com>
2022-02-19 16:38:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var box = createSprite(200,200,30,30)
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if (keyIsDown(RIGHT_ARROW))
+{
+box.position.x+=5
+
+}
+if (keyIsDown(LEFT_ARROW))
+{
+box.position.x-=5
+
+}
+if (keyIsDown(UP_ARROW))
+{
+box.position.y-=5
+
+}
+if (keyIsDown(DOWN_ARROW))
+{
+box.position.y+=5
+
+}
+
+
+
+
+
+
+drawSprites()
+}
+
+
+
+
